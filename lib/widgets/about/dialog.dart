40b54fb265bfcff2efd041045bd87385d2d9983c
Artem Chepurnoy <artemchep@gmail.com>
2019-05-09 12:44:57 +0300

    Use simple dialog as a base for About dialog

--- lib/widgets/about/dialog.dart
+++ lib/widgets/about/dialog.dart
@@ -6,41 +6,41 @@ import 'package:flutter/widgets.dart';
 class AboutDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
+    const padding = EdgeInsets.symmetric(horizontal: 24);
+
+    Padding HorizontalPadding(Widget child) {
+      return Padding(
+        padding: padding,
+        child: child,
+      );
+    }
+
+    return SimpleDialog(
       title: Text('About'),
-      content: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text('Game of Fifteen is a free and open source app '
-              'written with Flutter. It features beautiful design and '
-              'smooth animations.'),
-          const SizedBox(height: 8),
-          const Text('You can compete with your friends online. '
-              'The complexity of puzzles is similar from game to game.'),
-          const SizedBox(height: 24),
-          ListTile(
-            leading: Icon(Icons.code, size: 24),
-            title: const Text('Join development'),
-            onTap: () {
-              launchUrl(url: URL_REPOSITORY);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.bug_report, size: 24),
-            title: const Text('Send bug report'),
-            onTap: () {
-              launchUrl(url: URL_FEEDBACK);
-            },
-          ),
-        ],
-      ),
-      actions: <Widget>[
-        new FlatButton(
-          child: new Text("Close"),
-          onPressed: () {
-            Navigator.of(context).pop();
+      children: <Widget>[
+        HorizontalPadding(
+            const Text('Game of Fifteen is a free and open source app '
+                'written with Flutter. It features beautiful design and '
+                'smooth animations.')),
+        const SizedBox(height: 8),
+        HorizontalPadding(
+            const Text('You can compete with your friends online. '
+                'The complexity of puzzles is similar from game to game.')),
+        const SizedBox(height: 24),
+        ListTile(
+          leading: Icon(Icons.code, size: 24),
+          contentPadding: padding,
+          title: const Text('Join development'),
+          onTap: () {
+            launchUrl(url: URL_REPOSITORY);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.bug_report, size: 24),
+          contentPadding: padding,
+          title: const Text('Send bug report'),
+          onTap: () {
+            launchUrl(url: URL_FEEDBACK);
           },
         ),
       ],
